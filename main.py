56e6d2b707e830e7c9e6df3432c185a5320adf23
aidaalenezi <142677164+aidaalenezi@users.noreply.github.com>
2023-09-06 19:55:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 # write your code here
+person={ 'name': 'manal' ,
+        'age':43,
+        'hobbies':['singing', 'drawing','swiming']
+}
+
+print(person.get('name'))
+print(person.get('age'))
+
+person['age']=26
+person['country']='kuwait'
+print(person)
+
+print(len(person))
+  
+person['hobbies'].append('Dancing')
+
+def check_hobbies(person):
+    if len(person['hobbies'])>=3:
+     print('WOW YOU ARE AMAZING')
+
